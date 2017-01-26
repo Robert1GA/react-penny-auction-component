257a2d7793921aab7915ff9e4e6ae40aabbf54f6
Robert1GA <roberts.2015@yahoo.co.uk>
2017-01-26 09:34:29 -0800

    formatting. Need to run through setup next component.

---
+++ src/BidItem.jsx
@@ -0,0 +1,12 @@
+import React, { Component} from 'react';
+
+class BidItem extends Component {
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default BidItem;
---
+++ src/BidScreen.jsx
@@ -0,0 +1,197 @@
+import React, {Component} from 'react';
+import './BidScreen.css';
+
+class BidScreen extends Component {
+
+  render() {
+    return (
+      <div className="container">
+
+        <div className='item-wrapper'>
+          <div className='item-title'>
+            <p className='itemDesc'>$10 Shell Gift Card</p>
+          </div>
+          <div className='item-image'>
+            <img src="/img/$10 Shell Gift Card.jpg" />
+          </div>
+          <div className='item-time'>
+            0:00:00
+          </div>
+          <div className='item-price'>
+            $1.01
+          </div>
+          <div className='item-better'>
+            user123
+          </div>
+          <div className="bidBtn">
+            <button className='btn btn-warning'>Bid Now</button>
+          </div>
+        </div>
+
+        <div className="item-wrapper">
+          <div className='item-title'>
+            <p className='itemDesc'>$50 Walmart Gift Card</p>
+          </div>
+          <div className='item-image'>
+            <img src="/img/$50 Walmart Gift Card.jpg" />
+          </div>
+          <div className='item-time'>
+          </div>
+          <div className='item-price'>
+          </div>
+          <div className='item-better'>
+          </div>
+          <div className="bidBtn">
+            <button className='btn btn-warning'>Bid Now</button>
+          </div>
+        </div>
+
+        <div className="item-wrapper">
+          <div className='item-title'>
+            <p className='itemDesc'>15 Voucher Bids</p>
+          </div>
+          <div className='item-image'>
+            <img src="/img/15 Voucher Bids.jpg" />
+          </div>
+          <div className='item-time'>
+          </div>
+          <div className='item-price'>
+          </div>
+          <div className='item-better'>
+          </div>
+          <div className="bidBtn">
+            <button className='btn btn-warning'>Bid Now</button>
+          </div>
+        </div>
+
+        <div className="item-wrapper">
+          <div className='item-title'>
+            <p className='itemDesc'>250 Voucher Bids</p>
+          </div>
+          <div className='item-image'>
+            <img src="/img/250 Voucher Bids.jpg" />
+          </div>
+          <div className='item-time'>
+          </div>
+          <div className='item-price'>
+          </div>
+          <div className='item-better'>
+          </div>
+          <div className="bidBtn">
+            <button className='btn btn-warning'>Bid Now</button>
+          </div>
+        </div>
+
+        <div className="item-wrapper">
+          <div className='item-title'>
+            <p className='itemDesc'>50 Voucher Bids</p>
+          </div>
+          <div className='item-image'>
+            <img src="/img/50 Voucher Bids.jpg" />
+          </div>
+          <div className='item-time'>
+          </div>
+          <div className='item-price'>
+          </div>
+          <div className='item-better'>
+          </div>
+          <div className="bidBtn">
+            <button className='btn btn-warning'>Bid Now</button>
+          </div>
+        </div>
+
+        <div className="item-wrapper">
+          <div className='item-title'>
+            <p className='itemDesc'>Canon Pixma MG Series Wireless Printer</p>
+          </div>
+          <div className='item-image'>
+            <img src="/img/Canon Pixma MG Series Wireless Printer.jpg" />
+          </div>
+          <div className='item-time'>
+          </div>
+          <div className='item-price'>
+          </div>
+          <div className='item-better'>
+          </div>
+          <div className="bidBtn">
+            <button className='btn btn-warning'>Bid Now</button>
+          </div>
+        </div>
+
+        <div className="item-wrapper">
+          <div className='item-title'>
+            <p className='itemDesc'>Cuisinart Convection Bread Maker</p>
+          </div>
+          <div className='item-image'>
+            <img src="/img/Cuisinart Convection Bread Maker.jpg" />
+          </div>
+          <div className='item-time'>
+          </div>
+          <div className='item-price'>
+          </div>
+          <div className='item-better'>
+          </div>
+          <div className="bidBtn">
+            <button className='btn btn-warning'>Bid Now</button>
+          </div>
+        </div>
+
+        <div className="item-wrapper">
+          <div className='item-title'>
+            <p className='itemDesc'>Discrete "Hide a Key" Sprinkler Head</p>
+          </div>
+          <div className='item-image'>
+            <img src='/img/Discrete "Hide a Key" Sprinkler Head.jpg' />
+          </div>
+          <div className='item-time'>
+          </div>
+          <div className='item-price'>
+          </div>
+          <div className='item-better'>
+          </div>
+          <div className="bidBtn">
+            <button className='btn btn-warning'>Bid Now</button>
+          </div>
+        </div>
+
+        <div className='item-wrapper'>
+          <div className='item-title'>
+            <p className='itemDesc'>Ultra-Soft 1800 Series Sheet Set</p>
+          </div>
+          <div className='item-image'>
+            <img src="/img/Ultra-Soft 1800 Series Sheet Set.jpg" />
+          </div>
+          <div className='item-time'>
+          </div>
+          <div className='item-price'>
+          </div>
+          <div className='item-better'>
+          </div>
+          <div className="bidBtn">
+            <button className='btn btn-warning'>Bid Now</button>
+          </div>
+        </div>
+
+        <div className="item-wrapper">
+          <div className='item-title'>
+            <p className='itemDesc'>iPad Pro 9.7" 32GB WifFi</p>
+          </div>
+          <div className='item-image'>
+            <img src='/img/iPad Pro 9.7" 32GB WifFi.jpg' />
+          </div>
+          <div className='item-time'>
+          </div>
+          <div className='item-price'>
+          </div>
+          <div className='item-better'>
+          </div>
+          <div className="bidBtn">
+            <button className='btn btn-warning'>Bid Now</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BidScreen;
